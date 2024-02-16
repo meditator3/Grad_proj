aa09f7ddabe1afaf6cabc5be101cee7b7b0e839d
Ariel <r_e_l_g@yahoo.com>
2024-02-16 07:13:25 +0200

    Add files via upload

--- cloud-provider-aws.sh
+++ cloud-provider-aws.sh
@@ -2,5 +2,5 @@ ssh  ubuntu@$MASTER_K8S_IP_PUB <<EOF
 echo " installing AWS Cloud Controller Manager"
 helm repo add aws-cloud-controller-manager https://kubernetes.github.io/cloud-provider-aws
 helm repo update
-helm upgrade --install aws-cloud-controller-manager aws-cloud-controller-manager/aws-cloud-controller-manager
+helm upgrade --install aws-cloud-controller-manager aws-cloud-controller-manager/aws-cloud-controller-manager -f cloud-values.yaml
 EOF

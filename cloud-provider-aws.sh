9f13633364e7cfe908f76d7c7d85e9e6bbc4b645
Ariel <r_e_l_g@yahoo.com>
2024-02-17 12:46:36 +0200

    Update cloud-provider-aws.sh

--- cloud-provider-aws.sh
+++ cloud-provider-aws.sh
@@ -1,7 +1,7 @@
 MASTER_K8S_IP_PUB=$(terraform output -raw  master_ip_pub)
 ssh  ubuntu@$MASTER_K8S_IP_PUB <<EOF
 echo " installing AWS Cloud Controller Manager"
-helm repo add aws-cloud-controller-manager https://kubernetes.github.io/cloud-provider-aws
-helm repo update
-helm upgrade --install aws-cloud-controller-manager aws-cloud-controller-manager/aws-cloud-controller-manager -f cloud-values.yaml
+sudo helm repo add aws-cloud-controller-manager https://kubernetes.github.io/cloud-provider-aws
+sudo helm repo update
+sudo helm upgrade --install aws-cloud-controller-manager aws-cloud-controller-manager/aws-cloud-controller-manager -n kube-system -f cloud-values.yaml
 EOF

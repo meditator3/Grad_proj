1193c9e5917f2db404dd96e256635d6a16ccc4c2
Ariel <r_e_l_g@yahoo.com>
2024-02-17 22:24:04 +0200

    Update cloud-provider-aws.sh

--- cloud-provider-aws.sh
+++ cloud-provider-aws.sh
@@ -1,9 +1,11 @@
 MASTER_K8S_IP_PUB=$(terraform output -raw  master_ip_pub)
+scp cluster-autoscaler-autodiscover.yaml ubuntu@$MASTER_K8S_IP_PUB:~
 ssh  ubuntu@$MASTER_K8S_IP_PUB <<EOF
 echo " installing AWS Cloud Controller Manager"
 sudo helm repo add aws-cloud-controller-manager https://kubernetes.github.io/cloud-provider-aws
 sudo helm repo update
 sudo cp cloud-values.yaml /root/cloud-values.yaml
+sudo cp cluster-autoscaler-autodiscover.yaml /root/cluster-autoscaler-autodiscover.yaml
 sudo helm upgrade --install aws-cloud-controller-manager aws-cloud-controller-manager/aws-cloud-controller-manager -n kube-system -f cloud-values.yaml
 echo " installing cluster autoscaler using autodiscover configuration"
 sudo kubectl apply -f cluster-autoscaler-autodiscover.yaml

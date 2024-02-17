1d97f18ad01cd1fce2ca4d010d00b758b7248d93
Ariel <r_e_l_g@yahoo.com>
2024-02-17 21:11:00 +0200

    Update cloud-provider-aws.sh

--- cloud-provider-aws.sh
+++ cloud-provider-aws.sh
@@ -3,6 +3,7 @@ ssh  ubuntu@$MASTER_K8S_IP_PUB <<EOF
 echo " installing AWS Cloud Controller Manager"
 sudo helm repo add aws-cloud-controller-manager https://kubernetes.github.io/cloud-provider-aws
 sudo helm repo update
+cp cloud-values.yaml /root/cloud-values.yaml
 sudo helm upgrade --install aws-cloud-controller-manager aws-cloud-controller-manager/aws-cloud-controller-manager -n kube-system -f cloud-values.yaml
 echo " installing cluster autoscaler using autodiscover configuration"
 sudo kubectl apply -f cluster-autoscaler-autodiscover.yaml

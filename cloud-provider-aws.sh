7a8eb9f6f9d298880c34228fb39d808d90b93096
Ariel <r_e_l_g@yahoo.com>
2024-02-18 08:08:45 +0200

    Update cloud-provider-aws.sh

--- cloud-provider-aws.sh
+++ cloud-provider-aws.sh
@@ -1,9 +1,10 @@
 MASTER_K8S_IP_PUB=$(terraform output -raw  master_ip_pub)
 echo " copying autoscaler"
-scp cluster-autoscaler-autodiscover.yaml ubuntu@$MASTER_K8S_IP_PUB:~
+scp  -i ~/.ssh.id_rsa cluster-autoscaler-autodiscover.yaml   ubuntu@$MASTER_K8S_IP_PUB:~
 echo "finished copying autoscaler"
 echo "logging in"
-ssh  ubuntu@$MASTER_K8S_IP_PUB <<EOF
+ssh  -i ~/.ssh.id_rsa ubuntu@$MASTER_K8S_IP_PUB <<EOF
+echo "logged in"
 echo " installing AWS Cloud Controller Manager"
 sudo helm repo add aws-cloud-controller-manager https://kubernetes.github.io/cloud-provider-aws
 sudo helm repo update
